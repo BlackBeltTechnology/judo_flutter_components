d58bc3beef414a8ff83712bb14a9d771e9bfd2ce
Norbert Herczeg <noherczeg@gmail.com>
2021-09-15 11:08:22 +0200

    JNG-3043: fix time component init

--- lib/src/judo_time_input.dart
+++ lib/src/judo_time_input.dart
@@ -49,7 +49,7 @@ class _JudoTimeInputState extends State<JudoTimeInput> {
   void didUpdateWidget(JudoTimeInput oldWidget) {
     super.didUpdateWidget(oldWidget); // placement of this is SUPER IMPORTANT!
     if (controller.text != widget.initialDate) {
-      controller.text = widget.initialDate != null ? formatter.format(widget.initialDate) : null;
+      controller.text = widget.initialDate != null ? widget.initialDate.toString() : null;
     }
   }
 
