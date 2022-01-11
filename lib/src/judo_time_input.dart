3bbea4669a7c8f3daf8cc85bfb889834988ef9a9
rolandknobloch <roland.knobloch@blackbelt.hu>
2022-01-11 12:54:44 +0100

    JNG-3320 Updated JudoTime component

--- lib/src/judo_time_input.dart
+++ lib/src/judo_time_input.dart
@@ -6,16 +6,19 @@ class JudoTimeInput extends StatefulWidget {
     @required this.col,
     this.row = 1.0,
     this.label,
+    this.mandatory = false,
+    this.shortFormat = false,
     this.icon,
     this.onChanged,
     this.onSubmitted,
-    this.initialDate,
+    this.errorMessage,
+    this.initialTime,
     this.readOnly = false,
     this.disabled = false,
-    this.use24HourFormat,
     this.padding,
     this.stretch = false,
     this.alignment = Alignment.topLeft,
+    this.inCard = false,
     this.hidden = false,
     this.order,
   }) : super(key: key);
@@ -23,16 +26,20 @@ class JudoTimeInput extends StatefulWidget {
   final double col;
   final double row;
   final String label;
+  final bool mandatory;
+  /// if it's true, then it cuts seconds from time (hh:mm instead of hh:mm:ss format)
+  final bool shortFormat;
   final Icon icon;
   final Function onChanged;
   final Function onSubmitted;
-  final TimeOfDay initialDate;
+  final String errorMessage;
+  final String initialTime;
   final bool readOnly;
   final bool disabled;
-  final bool use24HourFormat;
   final bool stretch;
   final Alignment alignment;
   final EdgeInsets padding;
+  final bool inCard;
   final bool hidden;
   final double order;
 
@@ -42,19 +49,15 @@ class JudoTimeInput extends StatefulWidget {
 
 class _JudoTimeInputState extends State<JudoTimeInput> {
   final TextEditingController controller = TextEditingController();
-
+  // regexp for hh:mm:ss input format
+  final RegExp _regExp = RegExp(r"(^([01]?[0-9]?|2[0-3])$)|(^([01][0-9]|2[0-3]):[0-5]?[0-9]?$)|(^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5]?[0-9]?$)");
+  // regexp for hh:mm input format
+  final RegExp _shortFormatRegExp = RegExp(r"(^([01]?[0-9]?|2[0-3])$)|(^([01][0-9]|2[0-3]):[0-5]?[0-9]?$)");
+  
   @override
   void initState() {
     super.initState();
-    controller.text = widget.initialDate != null ? widget.initialDate.toString() : null;
-  }
-
-  @override
-  void didUpdateWidget(JudoTimeInput oldWidget) {
-    super.didUpdateWidget(oldWidget); // placement of this is SUPER IMPORTANT!
-    if (controller.text != widget.initialDate) {
-      controller.text = widget.initialDate != null ? widget.initialDate.toString() : null;
-    }
+    controller.text = widget.initialTime != null ? formatInitialTime(widget.initialTime) : null;
   }
 
   @override
@@ -65,67 +68,66 @@ class _JudoTimeInputState extends State<JudoTimeInput> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.hidden) return JudoSpacer(col: widget.col, row: widget.row);
+
+    final ThemeData theme = Theme.of(context);
     return JudoContainer(
-//      color: widget.disabled ? JudoComponentsSettings.disabledColor : null,
       padding: widget.padding ?? JudoComponentCustomizer.get().getDefaultPadding(),
       order: widget.order,
       col: widget.col,
       row: widget.row,
       stretch: widget.stretch,
       alignment: widget.alignment,
-      child: TextField(
-        controller: controller,
-        readOnly: widget.readOnly,
-        enabled: !widget.disabled,
-        decoration: widget.disabled ?
-        InputDecoration(
-          labelText: widget.label,
-          prefixIcon: widget.icon,
-          suffixIcon: iconDatePicker(context),
-        )
-            : widget.readOnly ?
-        InputDecoration(
-          labelText: widget.label,
-          prefixIcon: widget.icon,
-          border: InputBorder.none,
-          focusedBorder: InputBorder.none,
-          enabledBorder: InputBorder.none,
-          errorBorder: InputBorder.none,
-          disabledBorder: InputBorder.none,
-
-        )
-            :
-        InputDecoration(
-          labelText: widget.label,
-          prefixIcon: widget.icon,
-          suffixIcon: iconDatePicker(context),
+      child: Theme(
+        child: Container(
+          child: TextField(
+            controller: controller,
+            readOnly: widget.readOnly,
+            enabled: !widget.disabled,
+            decoration: JudoComponentCustomizer.get().getInputDecoration(
+                theme, widget.label, widget.icon,
+                (widget.disabled || widget.readOnly) ? null : iconDatePicker(context),
+                widget.mandatory, widget.disabled, widget.readOnly, widget.errorMessage,
+                hintText: widget.shortFormat ? 'hh:mm' : 'hh:mm:ss',
+            ),
+            onChanged: widget.onChanged,
+            onSubmitted: widget.onSubmitted,
+            inputFormatters: [
+              TextInputFormatter.withFunction((oldValue, newValue) {
+                RegExp correctRegExp = widget.shortFormat ? _shortFormatRegExp : _regExp;
+                var correctTextEditingValue = TextEditingValue();
+                if(correctRegExp.hasMatch(newValue.text)){
+                  correctTextEditingValue = TextEditingValue().copyWith(text: newValue.text, composing: newValue.composing, selection: newValue.selection);
+                } else {
+                  correctTextEditingValue = TextEditingValue().copyWith(text: oldValue.text, composing: oldValue.composing, selection: oldValue.selection);
+                }
+                return correctTextEditingValue;
+              }),
+            ],
+          ),
+          decoration: widget.errorMessage != null ? null : JudoComponentCustomizer.get().getInputBoxCustomizer(theme, widget.disabled, widget.readOnly),
         ),
-        onChanged: (value) => onChangedHandler(TimeOfDay.fromDateTime(DateTime.parse(value))),
-        onSubmitted: widget.onSubmitted,
+        data: JudoComponentCustomizer.get().getInputDateTimeThemeCustomizer(theme, widget.disabled, widget.readOnly, widget.inCard, widget.errorMessage),
       ),
     );
   }
 
   IconButton iconDatePicker(BuildContext context) {
-    var tempTime = widget.initialDate ?? DateTime.now();
     return IconButton(
         icon: Icon(
           Icons.calendar_today,
-//          color: widget.disabled ? JudoComponentsSettings.disabledColor : null,
         ),
-        onPressed: widget.disabled ? null : () async {
-          tempTime = await showTimePicker(
+        onPressed: (widget.disabled || widget.readOnly) ? null : () async {
+          TimeOfDay tempTime = await showTimePicker(
               context: context,
-              initialTime: tempTime,
+              initialTime: toTimeOfDay(widget.initialTime) ?? TimeOfDay.now(),
               initialEntryMode: TimePickerEntryMode.input,
-              builder: widget.use24HourFormat ? (BuildContext context, Widget child) {
+              builder: (BuildContext context, Widget child) {
                 return MediaQuery(
                   data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
                   child: child,
                 );
               }
-                  :
-              null
           );
           onChangedHandler(tempTime);
         }
@@ -134,8 +136,49 @@ class _JudoTimeInputState extends State<JudoTimeInput> {
 
   void onChangedHandler(TimeOfDay value) {
     if (widget.onChanged != null) {
-      controller.text = value.toString();
-      widget.onChanged(value);
+      if (value != null) {
+        controller.text = formatTimeOfDay(value);
+        widget.onChanged(formatTimeOfDay(value));
+      }
     }
+  }
+
+  String formatTimeOfDay(TimeOfDay value) {
+    String time = '';
+    time += value.hour > 9 ? value.hour.toString() : '0' + value.hour.toString();
+    time += ':';
+    time += value.minute > 9 ? value.minute.toString() : '0' + value.minute.toString();
+    if (!widget.shortFormat) {
+      time += ':00';
+    }
+    return time;
+  }
+
+  TimeOfDay toTimeOfDay(String time) {
+    if (time == null || time.isEmpty) return null;
+
+    List timeParts = time.split(':');
+    if (timeParts != null && timeParts.length > 1) {
+      int hour = int.tryParse(timeParts[0]);
+      int minute = int.tryParse(timeParts[1]);
+      if (hour != null && minute != null) {
+        return TimeOfDay(hour: hour, minute: minute);
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  // cuts seconds from the initial time
+  String formatInitialTime(String time) {
+    if (!widget.shortFormat) return time;
+
+    List timeParts = time.split(':');
+    if (timeParts.length > 2) {
+      timeParts.removeLast();
+    }
+    return timeParts.join(':');
   }
 }
